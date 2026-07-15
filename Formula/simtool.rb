30b92e42d7110ce6c4ae48dbd527273a43534837
Maksim Troshin <maksim.troshin@dif.tech>
2026-07-15 14:33:01 +0300

    Update simtool to v0.3.2

--- Formula/simtool.rb
+++ Formula/simtool.rb
@@ -1,9 +1,9 @@
 class Simtool < Formula
   desc "Stream and automate Apple Simulators"
   homepage "https://github.com/mstroshin/SimTool"
-  url "https://github.com/mstroshin/SimTool/releases/download/v0.3.1/simtool-v0.3.1-macos-arm64.tar.gz"
-  version "0.3.1"
-  sha256 "54ec9a3f29866398dace50d5dcb96cdf7a73fe640a3b17dac55995a8a4e6cb80"
+  url "https://github.com/mstroshin/SimTool/releases/download/v0.3.2/simtool-v0.3.2-macos-arm64.tar.gz"
+  version "0.3.2"
+  sha256 "fc4af914a0e4a811b4608b826f511721e0997354be0a3107d208dc1eb64ddcb1"
 
   def install
     bin.install "bin/simtool"
